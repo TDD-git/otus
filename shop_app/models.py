5088443a216d3f5d58df176f9db07ca35dba1f38
Dmitriy <ddtoropov@gmail.com>
2025-04-29 15:16:57 +0300

    model Product create

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
