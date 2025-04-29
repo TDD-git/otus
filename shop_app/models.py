81a006704b4b89b264e7e239ef40b2920b39385f
Dmitriy <ddtoropov@gmail.com>
2025-04-29 15:17:20 +0300

    model Category create

--- shop_app/models.py
+++ shop_app/models.py
@@ -12,5 +12,10 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
