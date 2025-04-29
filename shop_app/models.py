9b15bb47ac3733d6f5ded09bf6e07c66325b8b76
Dmitriy <ddtoropov@gmail.com>
2025-04-29 15:40:15 +0300

    foreignkey product - category

--- shop_app/models.py
+++ shop_app/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
         return self.name
